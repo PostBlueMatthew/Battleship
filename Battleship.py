68f4e36978f2651e885b69510ad0c9bb4889a8a0
Matthew Lawson <postbluematthew@gmail.com>
2021-02-10 23:41:22 -0500

    corrected typos in comments

--- Battleship.py
+++ Battleship.py
@@ -2,7 +2,7 @@ import random
 import time
 
 def build_board():                                                  #builds an 11 x 11 game board with a 10 x 10 playable space
-    board = {}                                                      #returns a dictionary with Keys = letters A - J, each dict entry corresponds to one row of bame goard
+    board = {}                                                      #returns a dictionary with Keys = letters A - J, each dict entry corresponds to one row of game board
     slots = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']      #board columns are numbered such that they are colated with array indicies for each dict entry
     swap = []
     for i in range(0, 11):
